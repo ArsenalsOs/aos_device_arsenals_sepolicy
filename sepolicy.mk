ca237551e87e13edee8b34fbef3f0bfd79bf9a21
Y-D-Lu <460999218@qq.com>
2024-05-12 20:47:45 +0800

    add platform_app sepolicy for OsArsenals to read battery, cpu, gpu infos TODO: get rid of platform_app domain, limit permission to certain domain

--- sepolicy.mk
+++ sepolicy.mk
@@ -9,6 +9,10 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
+ifneq (,$(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
+USE_SYSTEM_SEPOLICY_INSTEAD_VENDOR = true
+endif
+
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     device/arsenals/sepolicy/public
 
@@ -20,7 +24,13 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/arsenals/sepolicy/dynamic \
     device/arsenals/sepolicy/system
 else
+ifeq ($(USE_SYSTEM_SEPOLICY_INSTEAD_VENDOR), true)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/arsenals/sepolicy/dynamic \
+    device/arsenals/sepolicy/system
+else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/arsenals/sepolicy/dynamic \
     device/arsenals/sepolicy/vendor
+endif
 endif
